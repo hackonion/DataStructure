4ccaf04c1adb1faea43f4100a421184caa494381
hackonion <jorge21913@hotmail.com>
2020-11-15 22:10:38 -0600

    Fix TimSort

--- Sorting/TimSort.py
+++ Sorting/TimSort.py
@@ -1,4 +1,4 @@
-from Sorting.Merge_sort import merge
+from Merge_sort import merge
 
 
 def insert_sort(array, left=0, right=None):
@@ -28,19 +28,19 @@ def timSort(array):
         insert_sort(array,i,min((i + min_run -1),n - 1))
         size = min_run
         
-    while size < n:
-        for start in range(0,n,size * 2):
-            midpoint = start + size - 1
-            end = min((start + size * 2 - 1), (n-1))
-            
-            merged_array = merge(
-                left=array[start:midpoint + 1],
-                right=array[midpoint + 1:end + 1])
-            array[start:start + len(merged_array)] = merged_array
-        size *= 2
+        while size < n:
+            for start in range(0,n,size * 2):
+                midpoint = start + size - 1
+                end = min((start + size * 2 - 1), (n-1))
+                
+                merged_array = merge(
+                    left=array[start:midpoint + 1],
+                    right=array[midpoint + 1:end + 1])
+                array[start:start + len(merged_array)] = merged_array
+            size *= 2
     return array
 
 
 array =  [8, 2, 6, 4, 5,56,78,2,4,67,3]
 
-timSort(array)
+print(timSort(array))
